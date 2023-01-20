e67a4bf8b3d03148fcf61a6c4d8fb4401cf9820e
Sam Mitchell <samworth27@gmail.com>
2023-01-20 22:25:10 +1000

    forgot to import make key

--- aoc_util/graph_modules/__init__.py
+++ aoc_util/graph_modules/__init__.py
@@ -1,3 +1,3 @@
-from .graph import Node, Edge, Path, Graph
+from .graph import Node, Edge, Path, Graph, make_key
 from .graph_vis import default_config, random_path, random_position, draw_path, visualise_graph
 from .tsp_graph.tsp_graph import build_short_edges_first, TSPGraph, GeneticTSPGraph
